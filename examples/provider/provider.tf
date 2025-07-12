ce972f2e372e3db2ebbea4decad854e0aa995f0f
winebarrel <sugawara@winebarrel.jp>
2025-07-12 11:45:09 +0900

    Add docs

---
+++ examples/provider/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    multireplace = {
+      source = "winebarrel/multireplace"
+    }
+  }
+}
+
+output "london_bridge" {
+  value = provider::multireplace::multireplace(
+    "London Bridge Is Falling Down, Falling down, falling down",
+    { Falling = "Winding", falling = "jumping" }
+  )
+
+  # value = provider::multireplace::multireplace(
+  #   "London Bridge Is Falling Down, Falling down, falling down",
+  # 	{ Falling = "Winding" },
+  # 	{ falling = "jumping" }
+  # )
+}
+
+output "birmingham_bridge" {
+  value = provider::multireplace::multireplace(
+    "Birmingham Bridge Is Falling Down, Falling down, falling down",
+    { "/(?i)falling/" = "rising", "/(?i)down/" = "up" }
+  )
+}
+
+output "html" {
+  value = provider::multireplace::jsonunescape(
+    jsonencode({
+      link = "<a href=\"https://example.com?foo=bar&zoo=baz\">Open</a>"
+    })
+  )
+}
